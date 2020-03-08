f3b4f19c89f5f1f29448ef867169db5dd1eb480c
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-03-08 20:02:04 +0000

    upload all

--- scripts_ac3ac4/augmentation/rescale.py
+++ scripts_ac3ac4/augmentation/rescale.py
@@ -6,4 +6,24 @@ from skimage.transform import resize
 
 class Rescale(DataAugment):
     """
- 
+    Rescale augmentation.
+    
+    Args:
+        low (float): lower bound of the random scale factor. Default: 0.8
+        high (float): higher bound of the random scale factor. Default: 1.2
+        fix_aspect (bool): fix aspect ratio or not. Default: False
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self, low=0.8, high=1.2, fix_aspect=False, p=0.5):
+        super(Rescale, self).__init__(p=p) 
+        self.low = low
+        self.high = high
+        self.fix_aspect = fix_aspect
+
+        self.image_interpolation = 1
+        self.label_interpolation = 0
+        self.set_params()
+
+    def set_params(self):
+        assert (self.low >= 0.5)
+        a
