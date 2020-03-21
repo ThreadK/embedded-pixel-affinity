2e5b751396a581e6407b888450aee67344a4f472
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-03-21 12:04:45 +0000

    More text.

--- scripts_ac3ac4/data/data_transform.py
+++ scripts_ac3ac4/data/data_transform.py
@@ -17,4 +17,26 @@ def distance_transform_vol(label, quantize=True, mode='2d'):
             label, resolution=(1.0, 1.0, 1.0))
         if quantize:
             vol_distance = energy_quantize(vol_distance)
-        r
+        return vol_distance
+
+    vol_distance = []
+    vol_semantic = []
+    for i in range(label.shape[0]):
+        label_img = label[i].copy()
+        distance, semantic = distance_transform(label_img)
+        vol_distance.append(distance)
+        vol_semantic.append(semantic)
+
+    vol_distance = np.stack(vol_distance, 0)
+    vol_semantic = np.stack(vol_semantic, 0)
+    if quantize:
+        vol_distance = energy_quantize(vol_distance)
+        
+    return vol_distance
+
+def distance_transform(label, 
+                       bg_value: float = -1.0, 
+                       relabel: bool = True, 
+                       padding: bool = False,
+                       resolution: Tuple[int, ...] = (1.0, 1.0)):
+    """Euclidean distance transform
