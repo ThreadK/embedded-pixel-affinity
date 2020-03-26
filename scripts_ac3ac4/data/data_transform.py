0917afa607e85922de09ad085aecaff7089b62c7
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-03-26 06:27:17 +0000

    Run auto-package-update-now

--- scripts_ac3ac4/data/data_transform.py
+++ scripts_ac3ac4/data/data_transform.py
@@ -96,4 +96,22 @@ def energy_quantize(energy, levels=10):
 def decode_quantize(output, mode='max'):
     assert type(output) in [torch.Tensor, np.ndarray]
     assert mode in ['max', 'mean']
-    
+    if type(output) == torch.Tensor:
+        return _decode_quant_torch(output, mode)
+    else:
+        return _decode_quant_numpy(output, mode)
+
+def _decode_quant_torch(output, mode='max'):
+    # output: torch tensor of size (B, C, *)
+    if mode == 'max':
+        pred = torch.argmax(output, axis=1)
+        max_value = output.size()[1]
+        energy = pred / float(max_value)
+    elif mode == 'mean':
+        out_shape = output.shape
+        bins = np.array([0.1 * float(x-1) for x in range(11)])
+        bins = torch.from_numpy(bins.astype(np.float32))
+        bins = bins.view(1, -1, 1)
+        bins = bins.to(output.device)
+
+        output = output.view(out_shape[0], out_shape[1], -1) # (B, 
