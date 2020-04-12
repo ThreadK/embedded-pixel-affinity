32d2198d7bb652fd9409c50261cad7229817df97
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-04-12 15:50:31 +0000

    adding to site for testing

--- scripts_ac3ac4/model/squeeze_excite.py
+++ scripts_ac3ac4/model/squeeze_excite.py
@@ -18,4 +18,19 @@ class SELayer(nn.Module):
                 nn.Sigmoid())
 
     def forward(self, x):
-   
+        b, c, _, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1, 1)
+        return x * y
+
+class SELayerCS(nn.Module):
+    # Squeeze-and-excitation layer (channel & spatial)
+    def __init__(self, channel, reduction=4):
+        super(SELayerCS, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                SynchronizedBatchNorm1d(channel // reduction),
+                nn.ELU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                SynchronizedBatchNorm1d(ch
