16254fb01f6cf2ef8e6a49c0637e8704957899f8
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-05-01 19:08:42 +0000

    Adjust README.md

--- scripts_ac3ac4/utils/embeddings.py
+++ scripts_ac3ac4/utils/embeddings.py
@@ -29,4 +29,23 @@ def embedding_pca(embeddings, n_components=3, as_rgb=True):
 
 
 # TODO which slic implementation to use? vigra or skimage?
-# TODO can we run slic on the full embeddign space without RGB
+# TODO can we run slic on the full embeddign space without RGB PCA?
+# TODO is there a slic implementation that can do this?
+# TODO slic parameter?
+def embedding_slic(embeddings, run_pca=True):
+    """
+    """
+
+    if run_pca:
+        embeddings = embedding_pca(embeddings, as_rgb=True)
+    embeddings = embeddings.transpose((1, 2, 0)) if embeddings.ndim == 3 else\
+        embeddings.transpose((1, 2, 3, 0))
+    seg = slic(embeddings, convert2lab=True)
+    # print(embeddings.shape)
+    # seg = slicSuperpixels(embeddings[..., 0], intensityScaling=1., seedDistance=1)[0]
+    return seg
+
+
+def _embeddings_to_probabilities(embed1, embed2, delta, embedding_axis):
+    probs = (2 * delta - np.linalg.norm(embed1 - embed2, axis=embedding_axis)) / (2 * delta)
+   
