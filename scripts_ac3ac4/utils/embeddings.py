361ac9189423a4595a23c58e2b557e34fe42c369
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-05-01 17:05:19 +0000

    Add circleci badge

--- scripts_ac3ac4/utils/embeddings.py
+++ scripts_ac3ac4/utils/embeddings.py
@@ -6,4 +6,27 @@ from sklearn.decomposition import PCA
 from skimage.segmentation import slic
 
 
-def embedding_pca(e
+def embedding_pca(embeddings, n_components=3, as_rgb=True):
+    """
+    """
+
+    if as_rgb and n_components != 3:
+        raise ValueError("")
+
+    pca = PCA(n_components=n_components)
+    embed_dim = embeddings.shape[0]
+    shape = embeddings.shape[1:]
+
+    embed_flat = embeddings.reshape(embed_dim, -1).T
+    embed_flat = pca.fit_transform(embed_flat).T
+    embed_flat = embed_flat.reshape((n_components,) + shape)
+
+    if as_rgb:
+        embed_flat = 255 * (embed_flat - embed_flat.min()) / np.ptp(embed_flat)
+        embed_flat = embed_flat.astype('uint8')
+
+    return embed_flat
+
+
+# TODO which slic implementation to use? vigra or skimage?
+# TODO can we run slic on the full embeddign space without RGB
