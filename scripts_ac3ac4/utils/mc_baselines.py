c464a2fa76f67297237c281ea3586796b07af0c1
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-05-15 22:35:29 +0000

    agregando cambios

--- scripts_ac3ac4/utils/mc_baselines.py
+++ scripts_ac3ac4/utils/mc_baselines.py
@@ -11,4 +11,22 @@ import nifty.graph.opt.lifted_multicut as nlmc
 def find_matching_row_indices(x, y):
     assert isinstance(x, np.ndarray)
     assert isinstance(y, np.ndarray)
-    # using a dictionary, this is faster than the pure np varian
+    # using a dictionary, this is faster than the pure np variant
+    indices = []
+    rows_x = {tuple(row): i for i, row in enumerate(x)}
+    for i, row in enumerate(y):
+        if tuple(row) in rows_x:
+            indices.append([rows_x[tuple(row)], i])
+    return np.array(indices)
+
+
+def compute_mc_superpixels(affinities, n_threads):
+    segmenter = McSuperpixel(stacked_2d=True, n_threads=n_threads)
+    return segmenter(affinities)
+
+
+def compute_long_range_mc_superpixels(affinities, offsets,
+                                      only_repulsive_lr, n_threads,
+                                      stacked_2d=True):
+    segmenter = LongRangeMulticutSuperpixel(offsets=offsets, only_repulsive_lr=only_repulsive_lr,
+                                            stacke
