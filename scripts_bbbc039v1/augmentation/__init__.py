a9b106b7bb5138fca3700ebc460412a8b4cbaceb
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-06-06 10:02:30 +0000

    make file

---
+++ scripts_bbbc039v1/augmentation/__init__.py
@@ -0,0 +1,18 @@
+
+from .composition import Compose
+from .augmentor import DataAugment
+from .test_augmentor import TestAugmentor
+
+# augmentation methods
+from .warp import Elastic
+from .grayscale import Grayscale
+from .flip import Flip
+from .rotation import Rotate
+from .rescale import Rescale
+from .misalign import MisAlignment
+from .missing_section import MissingSection
+from .missing_parts import MissingParts
+from .motion_blur import MotionBlur
+from .cutblur import CutBlur
+from .cutnoise import CutNoise
+from .mixup import MixupAugmentor
