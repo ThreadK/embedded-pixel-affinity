3b0b05182989f34a4f76c84e33f8b1c2ade8bfd3
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-06-08 10:40:52 +0000

    color changes

--- scripts_bbbc039v1/augmentation/composition.py
+++ scripts_bbbc039v1/augmentation/composition.py
@@ -4,4 +4,21 @@ import warnings
 import numpy as np
 
 from skimage.morphology import dilation, erosion
-from skimage.f
+from skimage.filters import gaussian
+
+class Compose(object):
+    """Composing a list of data transforms. 
+    
+    The sample size of the composed augmentor can be larger than the 
+    specified input size of the model to ensure that all pixels are 
+    valid after center-crop.
+
+    Args:
+        transforms (list): list of transformations to compose
+        input_size (tuple): input size of model in :math:`(z, y, x)` order. Default: :math:`(8, 256, 256)`
+        smooth (bool): smoothing the object mask with Gaussian filtering. Default: True
+        keep_uncropped (bool): keep uncropped image and label. Default: False
+        keep_non_smooth (bool): keep the non-smoothed object mask. Default: False
+
+    Examples::
+        >>> augme
