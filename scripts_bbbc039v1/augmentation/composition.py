6b50292c4cf70440851d6260a32d6244d602c18e
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-06-08 12:26:28 +0000

    freezer: skip waking up tasks with PF_FREEZER_SKIP set

--- scripts_bbbc039v1/augmentation/composition.py
+++ scripts_bbbc039v1/augmentation/composition.py
@@ -21,4 +21,19 @@ class Compose(object):
         keep_non_smooth (bool): keep the non-smoothed object mask. Default: False
 
     Examples::
-        >>> augme
+        >>> augmentor = Compose([Rotate(p=1.0),
+        >>>                      Flip(p=1.0),
+        >>>                      Elastic(alpha=12.0, p=0.75),
+        >>>                      Grayscale(p=0.75),
+        >>>                      MissingParts(p=0.9)], 
+        >>>                      input_size = (8, 256, 256))
+        >>> data = {'image':input, 'label':label}
+        >>> augmented = augmentor(data)
+        >>> out_input, out_label = augmented['image'], augmented['label']
+    """
+    def __init__(self, 
+                 transforms, 
+                 input_size = (8,256,256),
+                 smooth = True,
+                 keep_uncropped = False,
+                 keep_non_s
