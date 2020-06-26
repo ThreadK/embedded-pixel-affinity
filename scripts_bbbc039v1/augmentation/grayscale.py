fec2535d67e4cf296dca9165234909cf06a4aa46
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-06-26 07:34:32 +0000

    v2.1.8

--- scripts_bbbc039v1/augmentation/grayscale.py
+++ scripts_bbbc039v1/augmentation/grayscale.py
@@ -5,4 +5,21 @@ class Grayscale(DataAugment):
     """Grayscale intensity augmentation, adapted from ELEKTRONN (http://elektronn.org/).
 
     Randomly adjust contrast/brightness, randomly invert the color space
-    
+    and apply gamma correction.
+
+    Args:
+        contrast_factor (float): intensity of contrast change. Default: 0.3
+        brightness_factor (float): intensity of brightness change. Default: 0.3
+        mode (string): one of ``'2D'``, ``'3D'`` or ``'mix'``. Default: ``'mix'``
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+
+    def __init__(self, contrast_factor=0.3, brightness_factor=0.3, mode='3D', p=0.5):
+        """Initialize parameters.
+        """
+        super(Grayscale, self).__init__(p=p)
+        self._set_mode(mode)
+        self.CONTRAST_FACTOR   = contrast_factor
+        self.BRIGHTNESS_FACTOR = brightness_factor
+
+    def set_params(s
