2993cf0c5065237bb9171fc1ac7c844599f44356
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-06-29 20:05:23 +0000

    Update dev requirements

--- scripts_bbbc039v1/augmentation/grayscale.py
+++ scripts_bbbc039v1/augmentation/grayscale.py
@@ -45,4 +45,25 @@ class Grayscale(DataAugment):
         Adapted from ELEKTRONN (http://elektronn.org/).
         """
         imgs = data['image']
-        transformedimgs
+        transformedimgs = np.copy(imgs)
+        ran = random_state.rand(transformedimgs.shape[-3]*3)
+
+        for z in range(transformedimgs.shape[-3]):
+            img = transformedimgs[z, :, :]
+            img *= 1 + (ran[z*3] - 0.5)*self.CONTRAST_FACTOR
+            img += (ran[z*3+1] - 0.5)*self.BRIGHTNESS_FACTOR
+            img = np.clip(img, 0, 1)
+            img **= 2.0**(ran[z*3+2]*2 - 1)
+            transformedimgs[z, :, :] = img
+
+        data['image'] = transformedimgs
+        return data    
+
+    def _augment3D(self, data, random_state=np.random):
+        """
+        Adapted from ELEKTRONN (http://elektronn.org/).
+        """
+        ran = random_state.rand(3)
+
+        imgs = data['image']
+        tran
