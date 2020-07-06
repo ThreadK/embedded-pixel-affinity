8350118f4ef6e76428ddbba3793b249ec18015a3
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-07-06 15:40:30 +0000

    Added screenshot.

--- scripts_bbbc039v1/augmentation/misalign.py
+++ scripts_bbbc039v1/augmentation/misalign.py
@@ -11,4 +11,23 @@ class MisAlignment(DataAugment):
         p (float): probability of applying the augmentation. Default: 0.5
     """
     def __init__(self, 
-                 displacement
+                 displacement=16, 
+                 rotate_ratio=0.0,
+                 p=0.5):
+        super(MisAlignment, self).__init__(p=p)
+        self.displacement = displacement
+        self.rotate_ratio = rotate_ratio
+        self.set_params()
+
+    def set_params(self):
+        self.sample_params['add'] = [0, 
+                                     int(math.ceil(self.displacement / 2.0)), 
+                                     int(math.ceil(self.displacement / 2.0))]
+
+    def misalignment(self, data, random_state):
+        images = data['image'].copy()
+        labels = data['label'].copy()
+
+        out_shape = (images.shape[0], 
+                     images.shape[1]-self.displacement, 
+           
