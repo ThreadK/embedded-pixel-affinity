46936b922194233bdab23e7066614c013e02612d
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-07-11 18:48:27 +0000

    rpmsg: make sure inflight messages don't invoke just-removed callbacks

--- scripts_bbbc039v1/augmentation/motion_blur.py
+++ scripts_bbbc039v1/augmentation/motion_blur.py
@@ -2,4 +2,26 @@ import cv2
 import math
 import random
 import numpy as np
-from .augmento
+from .augmentor import DataAugment
+
+class MotionBlur(DataAugment):
+    """Motion blur data augmentation of image stacks.
+    
+    Args:
+        sections (int): number of sections along z dimension to apply motion blur. Default: 2
+        kernel_size (int): kernel size for motion blur. Default: 11
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self, sections=2, kernel_size=11, p=0.5):
+        super(MotionBlur, self).__init__(p=p)
+        self.size = kernel_size
+        self.sections = sections
+        self.set_params()
+
+    def set_params(self):
+        # No change in sample size
+        pass
+
+    def motion_blur(self, data, random_state):
+        images = data['image'].copy()
+        l
