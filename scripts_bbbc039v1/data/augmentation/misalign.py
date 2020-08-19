6aa38dac4573e96d1ef43d2ec070a45998989ba6
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-08-19 16:14:17 +0000

    lab updates

--- scripts_bbbc039v1/data/augmentation/misalign.py
+++ scripts_bbbc039v1/data/augmentation/misalign.py
@@ -1,1 +1,24 @@
 import cv2
+import math
+import numpy as np
+from .augmentor import DataAugment
+
+class MisAlignment(DataAugment):
+    """Mis-alignment data augmentation of image stacks.
+    
+    Args:
+        displacement (int): maximum pixel displacement in `xy`-plane. Default: 16
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self, 
+                 displacement=16, 
+                 rotate_ratio=0.0,
+                 p=0.5):
+        super(MisAlignment, self).__init__(p=p)
+        self.displacement = displacement
+        self.rotate_ratio = rotate_ratio
+        self.set_params()
+
+    def set_params(self):
+        self.sample_params['add'] = [0, 
+                                     int(math.ce
