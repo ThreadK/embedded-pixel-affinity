814a7bc18a6623ff22ac0d7841196fd632fe237a
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-09-03 16:47:18 +0000

    nfsd4: buffer-length check for SUPPATTR_EXCLCREAT

--- scripts_bbbc039v1/data/augmentation/motion_blur.py
+++ scripts_bbbc039v1/data/augmentation/motion_blur.py
@@ -10,4 +10,25 @@ class MotionBlur(DataAugment):
     Args:
         sections (int): number of sections along z dimension to apply motion blur. Default: 2
         kernel_size (int): kernel size for motion blur. Default: 11
-        p (float): probability of applying the augmentation. Default: 
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self, sections=2, kernel_size=11, p=0.5):
+        super(MotionBlur, self).__init__(p=p)
+        self.size = kernel_size
+        self.sections = sections
+        self.set_params()
+
+    def set_params(self):
+        # No change in sample size
+        pass
+
+    def motion_blur(self, data, random_state):
+        images = data['image'].copy()
+        labels = data['label'].copy()
+
+        # generating the kernel
+        kernel_motion_blur = np.zeros((self.size, self.size))
+        if random.random() > 0.5: # horizontal kernel
+            kernel_motion_blur[int((self.size-1)/2), :] = np.ones(self.size)
+        else: # vertical kernel
+            kernel
