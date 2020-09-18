5e53e81ef7222c1409bb02218257070efcc677be
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-09-18 16:55:58 +0000

    Fix install.sh

--- scripts_bbbc039v1/data/augmentation/warp.py
+++ scripts_bbbc039v1/data/augmentation/warp.py
@@ -6,4 +6,20 @@ from .augmentor import DataAugment
 
 class Elastic(DataAugment):
     """Elastic deformation of images as described in [Simard2003]_ (with modifications).
-    The implementation is based on https:
+    The implementation is based on https://gist.github.com/erniejunior/601cdf56d2b424757de5.
+
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+        Convolutional Neural Networks applied to Visual Document Analysis", in
+        Proc. of the International Conference on Document Analysis and
+        Recognition, 2003.
+
+    Args:
+        alpha (float): maximum pixel-moving distance of elastic deformation. Default: 10.0
+        sigma (float): standard deviation of the Gaussian filter. Default: 4.0
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self,
+                 alpha=10.0,
+                 sigma=4.0,
+                 p=0.5):
+      
