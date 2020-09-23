1053de6051f75e089fe0dd1a2cdae0e086e7b5ec
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-09-23 14:05:05 +0000

    rename has.js to has-70898c7.js, phetsims/sherpa#14

--- scripts_bbbc039v1/data/data_affinity.py
+++ scripts_bbbc039v1/data/data_affinity.py
@@ -21,4 +21,19 @@ def mknhood2d(radius=1):
 
 def mknhood3d(radius=1):
     # Makes nhood structures for some most used dense graphs.
-    #
+    # The neighborhood reference for the dense graph representation we use
+    # nhood(1,:) is a 3 vector that describe the node that conn(:,:,:,1) connects to
+    # so to use it: conn(23,12,42,3) is the edge between node [23 12 42] and [23 12 42]+nhood(3,:)
+    # See? It's simple! nhood is just the offset vector that the edge corresponds to.
+
+    ceilrad = np.ceil(radius)
+    x = np.arange(-ceilrad,ceilrad+1,1)
+    y = np.arange(-ceilrad,ceilrad+1,1)
+    z = np.arange(-ceilrad,ceilrad+1,1)
+    [i,j,k] = np.meshgrid(z,y,x)
+
+    idxkeep = (i**2+j**2+k**2)<=radius**2
+    i=i[idxkeep].ravel(); j=j[idxkeep].ravel(); k=k[idxkeep].ravel();
+    zeroIdx = np.array(len(i) // 2).astype(np.int32);
+
+    nhood = np.vstack((k[:z
