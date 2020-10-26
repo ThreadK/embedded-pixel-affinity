e2c5a56c84e8330e29161e11fc7ca0280a5ecefa
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-10-26 22:46:33 +0000

    Adding source code

--- scripts_bbbc039v1/data/data_segmentation.py
+++ scripts_bbbc039v1/data/data_segmentation.py
@@ -22,4 +22,28 @@ def getSegType(mid):
 def relabel(seg, do_type=False):
     # get the unique labels
     uid = np.unique(seg)
-    # ignore all-b
+    # ignore all-background samples
+    if len(uid)==1 and uid[0] == 0:
+        return seg
+
+    uid = uid[uid > 0]
+    mid = int(uid.max()) + 1 # get the maximum label for the segment
+
+    # create an array from original segment id to reduced id
+    m_type = seg.dtype
+    if do_type:
+        m_type = getSegType(mid)
+    mapping = np.zeros(mid, dtype=m_type)
+    mapping[uid] = np.arange(1, len(uid) + 1, dtype=m_type)
+    return mapping[seg]
+
+def remove_small(seg, thres=100):
+    sz = seg.shape
+    seg = seg.reshape(-1)
+    uid, uc = np.unique(seg, return_counts=True)
+    seg[np.in1d(seg,uid[uc<thres])] = 0
+    return seg.reshape(sz)
+
+def im2col(A, BSZ, stepsize=1):
+    # Parameters
+    M,N = A.shape
