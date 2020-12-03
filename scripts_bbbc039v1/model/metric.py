890acac0775cd60f460ae2f84c0e977d785471c7
ThreadK <155293762+ThreadK@users.noreply.github.com>
2020-12-03 07:29:57 +0000

    staging: android: ashmem: add missing include

--- scripts_bbbc039v1/model/metric.py
+++ scripts_bbbc039v1/model/metric.py
@@ -18,4 +18,23 @@ class MeanIoU:
         :param ignore_index: id of the label to be ignored from IoU computation
         """
         self.ignore_index = ignore_index
-        self.skip
+        self.skip_channels = skip_channels
+
+    def __call__(self, input, target):
+        """
+        :param input: 5D probability maps torch float tensor (NxCxDxHxW)
+        :param target: 4D or 5D ground truth torch tensor. 4D (NxDxHxW) tensor will be expanded to 5D as one-hot
+        :return: intersection over union averaged over all channels
+        """
+        assert input.dim() == 5
+
+        n_classes = input.size()[1]
+
+        if target.dim() == 4:
+            target = expand_as_one_hot(target, C=n_classes, ignore_index=self.ignore_index)
+
+        assert input.size() == target.size()
+
+        per_batch_iou = []
+        for _input, _target in zip(input, target):
+            binary_prediction = self._binarize_predictions(_input
