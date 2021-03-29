62f3a5c82011fc3ec1b19c4e3db0665f9191f6e4
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-03-29 13:05:16 +0000

    netfilter: nfnetlink_cthelper: Remove 'const' and '&' to avoid warnings

--- scripts_bbbc039v1/utils/cluster.py
+++ scripts_bbbc039v1/utils/cluster.py
@@ -16,4 +16,25 @@ def iou(gt, seg):
 
 
 def expand_labels_watershed(seg, raw, erosion_iters=4):
-    b
+    bg_mask = seg == 0
+    # don't need to  do anything if we only have background
+    if bg_mask.size == int(bg_mask.sum()):
+        return seg
+
+    hmap = vigra.filters.gaussianSmoothing(raw, sigma=1.)
+
+    bg_mask = binary_erosion(bg_mask, iterations=erosion_iters)
+    seg_new = seg.copy()
+    bg_id = int(seg.max()) + 1
+    seg_new[bg_mask] = bg_id
+
+    seg_new, _ = vigra.analysis.watershedsNew(hmap, seeds=seg_new.astype('uint32'))
+
+    seg_new[seg_new == bg_id] = 0
+    return seg_new
+
+
+def cluster(emb, clustering_alg, semantic_mask=None):
+    output_shape = emb.shape[1:]
+    # reshape (E, D, H, W) -> (E, D * H * W) and transpose -> (D * H * W, E)
+    flattened_embeddings = emb.reshape(emb.sha
