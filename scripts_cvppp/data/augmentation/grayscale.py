abc3931b0901ba2cdf176d215cb59d747a9a8976
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-05-26 22:20:37 +0000

    First cut

--- scripts_cvppp/data/augmentation/grayscale.py
+++ scripts_cvppp/data/augmentation/grayscale.py
@@ -2,4 +2,18 @@ import numpy as np
 from .augmentor import DataAugment
 
 class Grayscale(DataAugment):
-    """Grayscale intensity au
+    """Grayscale intensity augmentation, adapted from ELEKTRONN (http://elektronn.org/).
+
+    Randomly adjust contrast/brightness, randomly invert the color space
+    and apply gamma correction.
+
+    Args:
+        contrast_factor (float): intensity of contrast change. Default: 0.3
+        brightness_factor (float): intensity of brightness change. Default: 0.3
+        mode (string): one of ``'2D'``, ``'3D'`` or ``'mix'``. Default: ``'mix'``
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+
+    def __init__(self, contrast_factor=0.3, brightness_factor=0.3, mode='mix', p=0.5):
+        """Initialize parameters.
+        "
