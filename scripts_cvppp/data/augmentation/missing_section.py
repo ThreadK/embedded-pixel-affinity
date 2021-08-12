c707510668c095634a3d31c7eb5a386c307f88c1
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-08-12 13:22:19 +0000

    Create home.php

--- scripts_cvppp/data/augmentation/missing_section.py
+++ scripts_cvppp/data/augmentation/missing_section.py
@@ -15,4 +15,20 @@ class MissingSection(DataAugment):
         self.set_params()
 
     def set_params(self):
-        self.sample_params['add'] = [int(math.ceil(self.n
+        self.sample_params['add'] = [int(math.ceil(self.num_sections / 2.0)), 0, 0]
+
+    def missing_section(self, data, random_state):
+        images, labels = data['image'], data['label']
+        new_images = images.copy()   
+        new_labels = labels.copy()
+
+        idx = random_state.choice(np.array(range(1, images.shape[0]-1)), self.num_sections, replace=False)
+
+        new_images = np.delete(new_images, idx, 0)
+        new_labels = np.delete(new_labels, idx, 0)
+
+        return new_images, new_labels
+    
+    def __call__(self, data, random_state=np.random):
+        new_images, new_labels = self.missing_section(data, random_state)
+        return {'image': new_images, 'label': new_labels}
