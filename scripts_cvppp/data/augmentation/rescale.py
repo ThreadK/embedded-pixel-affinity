a47cd486f060fcdfe10845485b5f057b7172c611
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-08-18 15:31:33 +0000

    Installation notes

--- scripts_cvppp/data/augmentation/rescale.py
+++ scripts_cvppp/data/augmentation/rescale.py
@@ -1,2 +1,23 @@
 from __future__ import division
-import 
+import cv2
+import numpy as np
+from .augmentor import DataAugment
+from skimage.transform import resize
+
+class Rescale(DataAugment):
+    """
+    Rescale augmentation.
+    
+    Args:
+        low (float): lower bound of the random scale factor. Default: 0.8
+        high (float): higher bound of the random scale factor. Default: 1.2
+        fix_aspect (bool): fix aspect ratio or not. Default: False
+        p (float): probability of applying the augmentation. Default: 0.5
+    """
+    def __init__(self, low=0.8, high=1.2, fix_aspect=False, p=0.5):
+        super(Rescale, self).__init__(p=p) 
+        self.low = low
+        self.high = high
+        self.fix_aspect = fix_aspect
+
+        self.i
