390a5b07492eb8b359beacf89894de25a9ed084a
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-08-18 19:22:49 +0000

    A random commit message

--- scripts_cvppp/data/augmentation/rescale.py
+++ scripts_cvppp/data/augmentation/rescale.py
@@ -20,4 +20,24 @@ class Rescale(DataAugment):
         self.high = high
         self.fix_aspect = fix_aspect
 
-        self.i
+        self.image_interpolation = 1
+        self.label_interpolation = 0
+        self.set_params()
+
+    def set_params(self):
+        assert (self.low >= 0.5)
+        assert (self.low <= 1.0)
+        ratio = 1.0 / self.low
+        self.sample_params['ratio'] = [1.0, ratio, ratio]
+
+    def random_scale(self, random_state):
+        rand_scale = random_state.rand() * (self.high - self.low) + self.low
+        return rand_scale
+
+    def apply_rescale(self, image, label, sf_x, sf_y, random_state):
+        # apply image and mask at the same time
+        transformed_image = image.copy()
+        transformed_label = label.copy()
+
+        y_length = int(sf_y * image.shape[1])
+        if y_length <= image.shape[1
