aa792ca0d1065e241b74dd2b3a51942840906bd4
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-11-01 16:02:16 +0000

    Add ga

--- scripts_cvppp/model/metric.py
+++ scripts_cvppp/model/metric.py
@@ -1,3 +1,25 @@
 import sys
 sys.path.insert(1, '/ghome/liuxy/BCE3')
-from medpy.metric.binary import 
+from medpy.metric.binary import dc
+import torch
+
+def dc_score(output,target):
+    score = dc(output,target)
+    return score
+
+class MeanIoU:
+    """
+    Computes IoU for each class separately and then averages over all classes.
+    """
+
+    def __init__(self, skip_channels=(), ignore_index=None, **kwargs):
+        """
+        :param skip_channels: list/tuple of channels to be ignored from the IoU computation
+        :param ignore_index: id of the label to be ignored from IoU computation
+        """
+        self.ignore_index = ignore_index
+        self.skip_channels = skip_channels
+
+    def __call__(self, input, target):
+        """
+        :param input: 5D probability maps torch float tensor (NxCxDxHxW
