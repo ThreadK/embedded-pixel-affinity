8bc78438cbe6b7a123cc5f80f8d41890aa7693b1
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-11-04 20:33:50 +0000

    improve error logging

--- scripts_cvppp/model/metric.py
+++ scripts_cvppp/model/metric.py
@@ -22,4 +22,22 @@ class MeanIoU:
 
     def __call__(self, input, target):
         """
-        :param input: 5D probability maps torch float tensor (NxCxDxHxW
+        :param input: 5D probability maps torch float tensor (NxCxDxHxW)
+        :param target: 4D or 5D ground truth torch tensor. 4D (NxDxHxW) tensor will be expanded to 5D as one-hot
+        :return: intersection over union averaged over all channels
+        """
+        assert input.dim() == 5
+
+        n_classes = input.size()[1]
+
+        if target.dim() == 4:
+            target = expand_as_one_hot(target, C=n_classes, ignore_index=self.ignore_index)
+
+        assert input.size() == target.size()
+
+        per_batch_iou = []
+        for _input, _target in zip(input, target):
+            binary_prediction = self._binarize_predictions(_input, n_classes)
+
+            if self.ignore_index is not None:
+                # ze
