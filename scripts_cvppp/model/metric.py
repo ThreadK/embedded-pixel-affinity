6fbdfc316d9e0a2b77cc46c7738e0f6680bda76f
ThreadK <155293762+ThreadK@users.noreply.github.com>
2021-12-01 09:27:13 +0000

    chore(package): Update dependencies

--- scripts_cvppp/model/metric.py
+++ scripts_cvppp/model/metric.py
@@ -93,4 +93,21 @@ def expand_as_one_hot(input, C, ignore_index=None):
 
     # expand the input tensor to Nx1xDxHxW before scattering
     input = input.unsqueeze(1)
-    # cr
+    # create result tensor shape (NxCxDxHxW)
+    shape = list(input.size())
+    shape[1] = C
+
+    if ignore_index is not None:
+        # create ignore_index mask for the result
+        mask = input.expand(shape) == ignore_index
+        # clone the src tensor and zero out ignore_index in the input
+        input = input.clone()
+        input[input == ignore_index] = 0
+        # scatter to get the one-hot tensor
+        result = torch.zeros(shape).to(input.device).scatter_(1, input, 1)
+        # bring back the ignore_index in the result
+        result[mask] = ignore_index
+        return result
+    else:
+        # scatter to get the one-hot tensor
+        return torch.zeros(shape).to(input.device).scatter
