a223d0c4c0074b5f6e5d9ac74b8c21128bb2831d
ThreadK <155293762+ThreadK@users.noreply.github.com>
2022-03-22 21:30:35 +0000

    Fixed encoding

--- scripts_cvppp/utils/merge_small.py
+++ scripts_cvppp/utils/merge_small.py
@@ -172,4 +172,21 @@ def merge_small_segments(mc_seg, min_seg_size):
     # merge the nodes with udf
     udf = UnionFind(n_nodes + 1)
     for merge_pair in merge_nodes:
-        udf.merge(merg
+        udf.merge(merge_pair[0], merge_pair[1])
+
+    # get new to old as merge result
+    new_to_old = udf.get_merge_result()
+
+    # find old to new nodes
+    old_to_new = np.zeros( n_nodes + 1, dtype = np.uint32 )
+    for set_id in range(len(new_to_old)):
+        for n_id in new_to_old[set_id]:
+            assert n_id <= n_nodes, str(n_id) + " , " + str(n_nodes)
+            old_to_new[n_id] = set_id
+
+    # merge the new nodes
+    merged_seg = seg_rag.projectLabelsToBaseGraph(old_to_new)
+    # print(merged_seg.shape)
+    # print(merged_seg.dtype)
+    # merged_seg = vigra.analysis.labelVolume(merged_seg)
+    return merged_seg
---
+++ scripts_cvppp/utils/postprocess.py
@@ -0,0 +1,2 @@
+import numpy as np
+from sklearn.cluster import MeanShift, es
